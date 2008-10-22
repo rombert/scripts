ed1518af8703b41a735b3fe4771584bb0e1ff220
Robert Munteanu <robert.munteanu@gmail.com>
2008-10-23 00:05:48 +0300

    Added directory arg support to find_duplicates_in_jars

--- bin/find_duplicates_in_jars.sh
+++ bin/find_duplicates_in_jars.sh
@@ -2,7 +2,16 @@
 
 # picked up from http://talk-on-tech.blogspot.com/2008/10/spotting-duplicate-classes-in-jar-files.html
 
-for lib in `find . -name '*.jar'`; do
+if [ $# == 0 ]; then
+	dir='.'
+elif [ $# == 1 ]; then 
+	dir=$1
+else
+	echo "Usage: $0 [dir]";
+	exit 1;
+fi
+
+for lib in `find $dir -name '*.jar'`; do
   for class in `unzip -l $lib | egrep -o '[^ ]*.class$'`; do
     class=`echo $class | sed s/\\\\.class// | sed s/[-.\\/$]/_/g`
     existing=$( eval "echo \$CLS_${class}" )
