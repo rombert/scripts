12f8cb2ca9f1b923e0d23ad0adde283daca8761b
Robert Munteanu <robert.munteanu@gmail.com>
2009-03-13 15:00:24 +0200

    improve + quiet down the generate password script

--- bin/generate_password.sh
+++ bin/generate_password.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# courtesy of http://it.slashdot.org/comments.pl?sid=1119941&cid=26765305
-dd if=/dev/random bs=200 count=1 | tr -cd 'A-Za-z0-9!@#$%^&*()_+'; echo
+# courtesy of http://it.slashdot.org/comments.pl?sid=1119941&cid=26767103
+dd status=noxfer if=/dev/random bs=1 count=24 | base64 
