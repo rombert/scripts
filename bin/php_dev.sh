1b48cbf3855c0133bf0265856b49aff94fd023c0
Robert Munteanu <robert@lmn.ro>
2012-12-15 23:50:36 +0200

    php_dev: update script for 12.2

--- bin/php_dev.sh
+++ bin/php_dev.sh
@@ -14,12 +14,14 @@ fi
 
 function start_service {
 	echo -n "Starting $1... " ;
-	sudo /sbin/service $1 start;
+	sudo systemctl start $1.service;
+	echo "";
 }
 
 function stop_service {
 	echo -n "Stopping $1... " ;
-	sudo /sbin/service $1 stop;
+	sudo systemctl stop $1.service;
+	echo "";
 }
 
 sudo -v; # cache password
@@ -27,9 +29,7 @@ sudo -v; # cache password
 if [ $mode == 'on' ]; then
 	start_service apache2;
 	start_service mysql;
-	start_service postfix;
 else
 	stop_service apache2;
 	stop_service mysql;
-	stop_service postfix;
 fi
