642a70d0c692c8de59a9591a35c1ed90439d9a30
Robert Munteanu <robert@lmn.ro>
2014-11-10 22:49:13 +0200

    list_patches: better handle case where no patches exist

--- bin/list_patches.sh
+++ bin/list_patches.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-zypper patch-info $(sudo zypper -n lu -t patch | awk -F '|'  '$2 ~ /openSUSE/ {print $2}' | tr -d '\n') | $PAGER
+patches=$(sudo zypper -n lu -t patch | awk -F '|'  '$2 ~ /openSUSE/ {print $2}' | tr -d '\n')
+
+if [ X$patches = 'X' ]; then
+    exit 0;
+fi
+
+zypper patch-info $patches | $PAGER
