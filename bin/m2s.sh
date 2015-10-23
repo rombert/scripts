b9bc5eddd39957d5d4556224222300bfef84b530
Robert Munteanu <robert@lmn.ro>
2015-10-23 17:01:02 +0300

    m2s : simplify interface

--- bin/m2s.sh
+++ bin/m2s.sh
@@ -1,66 +1,49 @@
 #!/bin/sh
 
 usage() {
-    echo "Usage: $0 ls|current"
-    echo "Usage; $0 select name"
+    echo "Usage: $0 [new-settings]"
 
     exit 1
 }
 
-case "$1" in
-    ls)
-        for file in ~/.m2/settings-*.xml; do
-            if [ $file = ~/.m2/settings-security.xml ]; then
-                continue;
-            fi
-
-            echo -n $file;
-
-            if [ -h ~/.m2/settings.xml ] && [ $file = $(readlink -f ~/.m2/settings.xml) ] ; then
-                echo  " [x]";
-            else
-                echo "";
-            fi
-        done
-    ;;
-
-    current)
-
-        if [ ! -h ~/.m2/settings.xml ] ; then
-            echo "~/.m2/settings.xml is not a symlink"
-            exit 4
+if [ $# -eq 0 ]; then
+   for file in ~/.m2/settings-*.xml; do
+        if [ $file = ~/.m2/settings-security.xml ]; then
+            continue;
         fi
 
-        readlink -f ~/.m2/settings.xml
-    ;;
+        echo -n $file;
 
-    select)
-
-        if [ $# -ne 2 ]; then
-            usage
-        fi
-
-        if [ $2 = 'security' ]; then
-            echo "Invalid choice : $2"
-            exit 3
-        fi
-
-        if [ ! -h ~/.m2/settings.xml ]; then
-            echo "Unable to manage settings.xml since it's not a symlink"
-            exit 4
-        fi
-
-        CFG=~/.m2/settings-$2.xml
-
-        if [ -f $CFG ] ; then
-            ln -sf $CFG ~/.m2/settings.xml
+        if [ -h ~/.m2/settings.xml ] && [ $file = $(readlink -f ~/.m2/settings.xml) ] ; then
+            echo  " [x]";
         else
-            echo "No file $CFG to select"
-            exit 2
+            echo "";
         fi
-    ;;
+    done
 
-    *)
-        usage
-    ;;
-esac
+elif [ $# -eq 1 ]; then
+    if [ $1 = 'security' ]; then
+        echo "Invalid choice : $1"
+        exit 3
+    fi
+
+    if [ ! -h ~/.m2/settings.xml ]; then
+        echo "Unable to manage settings.xml since it's not a symlink"
+        exit 4
+    fi
+
+    CFG=~/.m2/settings-$1.xml
+
+    if [ -f $CFG ] ; then
+        ln -sf $CFG ~/.m2/settings.xml
+    else
+        echo "No file $CFG to select"
+        exit 2
+    fi
+
+else
+    usage
+fi
+
+
+
