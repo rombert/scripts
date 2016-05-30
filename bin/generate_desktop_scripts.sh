6356e1ebdd17b2c4e2255bf65a098951a75eddf0
Robert Munteanu <robert@lmn.ro>
2016-05-30 12:20:09 +0300

    generate_desktop_scripts: only pick up one icon if more exist

--- bin/generate_desktop_scripts.sh
+++ bin/generate_desktop_scripts.sh
@@ -2,7 +2,7 @@
 
 if [ -f /opt/eclipse/eclipse ]; then
     SCRATCH_DIR=$(mktemp -d);
-    ICON_FILE=$(find /opt/eclipse -name eclipse256.png)
+    ICON_FILE=$(find /opt/eclipse -name eclipse256.png | head -n 1)
     echo "[Desktop Entry]
 Type=Application
 Version=1.0
